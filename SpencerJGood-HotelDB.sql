b27a7c3f4f24d42296818029dc4c1d30b6594ef8
SJGood96 <56788071+SJGood96@users.noreply.github.com>
2021-01-21 10:10:10 -0500

    Add files via upload
    This was the second draft of this due to running into a lot of errors when putting data into it

--- SpencerJGood-HotelDB.sql
+++ SpencerJGood-HotelDB.sql
@@ -15,59 +15,86 @@ CREATE TABLE IF NOT EXISTS Guest(
 	City VARCHAR (45) NOT NULL,
 	State CHAR(2) NOT NULL,
 	Zip CHAR (5),
-	Phone CHAR (11),
+	Phone CHAR (14),
     PRIMARY KEY (GuestID));
+    
+-- Creation of the Amenities table -- 
 
 CREATE TABLE IF NOT EXISTS Amenities (
-	AmenitiesID INT NOT NULL auto_increment,
+	AmenitiesID INT NOT NULL,
 	AmenitiesName VARCHAR (45) NOT NULL,
     PRIMARY KEY (AmenitiesID));
     
+-- Creation of the RoomType Table --     
+    
 CREATE TABLE IF NOT EXISTS RoomType (
-	RTypeID INT NOT NULL,
-    ADA VARCHAR (3) NOT NULL,
+	RType VARCHAR (6) NOT NULL,
     StandOcc CHAR(1) NOT NULL, 
     MaxOcc Char(1) NOT NULL,
-    PRIMARY KEY (RTypeID));
+    PRIMARY KEY (RType));
+
+    -- Creation of the Room table --
     
 CREATE TABLE IF NOT EXISTS Room (
-	RoomID INT NOT NULL auto_increment,
-    RTypeID INT NOT NULL,
-    ReservationID INT NOT NULL,
+	RoomID INT NOT NULL,
+    RType VARCHAR (6) NOT NULL,
+    ADA VARCHAR (3) NOT NULL,
 	BasePrice DECIMAL (5,2) NOT NULL,
-	MaxPrice DECIMAL (5,2) NOT NULL,
+    ExtraPerson VARCHAR (2) NOT NULL,
 	PRIMARY KEY (RoomID));
 
 -- Adding Foreign keys --
 ALTER TABLE Room
-	ADD CONSTRAINT fk_RTypeID
-	FOREIGN KEY (RTypeID) 
-	REFERENCES RoomType (RTypeID); 
+	ADD CONSTRAINT fk_RType
+	FOREIGN KEY (RType) 
+	REFERENCES RoomType (RType) ON DELETE NO ACTION; 
+
+CREATE TABLE IF NOT EXISTS Room_has_Amenities (
+Room_RoomID INT NOT NULL,
+Amenities_AmenitiesID INT NOT NULL,
+PRIMARY KEY (Room_RoomID, Amenities_AmenitiesID));
 
 
+-- Adding Foreign keys --
+ALTER TABLE Room_has_Amenities
+	ADD CONSTRAINT fk_Room_RoomID
+	FOREIGN KEY (Room_RoomID) 
+	REFERENCES Room (RoomID) ON DELETE NO ACTION;
+    
+Alter TABLE Room_has_Amenities
+	ADD CONSTRAINT fk_Amenities_AmenitiesID
+    FOREIGN KEY (Amenities_AmenitiesID)
+    REFERENCES Amenities (AmenitiesID);
+    
 -- creation of the reservation table --
 CREATE TABLE IF NOT EXISTS Reservation (
 	ReservationID INT NOT NULL auto_increment,
-    RoomID INT NOT NULL,
     GuestID INT NOT NULL,
 	Adults VARCHAR(2) NOT NULL,
 	Children VARCHAR(2) NOT NULL,
 	StartDate DATE NOT NULL,
 	EndDate DATE NOT NULL,
+    TotalPrice DECIMAL(7,2) NOT NULL,
     PRIMARY KEY (ReservationID));
 
 -- Adding Foreign Keys--
 ALTER TABLE Reservation
 	ADD CONSTRAINT fk_GuestID
 	FOREIGN KEY (GuestID)
-    REFERENCES Guest (GuestID);
-    
-ALTER TABLE Room
-	ADD CONSTRAINT fk_ReservationID
-    FOREIGN KEY (ReservationID)
-    REFERENCES Reservation (ReservationID);
+    REFERENCES Guest (GuestID)ON DELETE NO ACTION;
 
-ALTER TABLE Reservation
-	ADD CONSTRAINT fk_RoomID
-	FOREIGN KEY (RoomID)
-    REFERENCES Room (RoomID);
+
+CREATE TABLE IF NOT EXISTS Room_has_Reservation (
+Room_Reservation_RoomID INT NOT NULL,
+Reservation_ReservationID INT NOT NULL,
+PRIMARY KEY (Room_Reservation_RoomID, Reservation_ReservationID));
+
+ALTER TABLE Room_has_Reservation
+	ADD CONSTRAINT fk_Reservation_ReservationID
+	FOREIGN KEY (Reservation_ReservationID) 
+	REFERENCES Reservation (ReservationID) ON DELETE NO ACTION;
+    
+Alter TABLE Room_has_Reservation
+	ADD CONSTRAINT fk_Room_Reservation_RoomID
+    FOREIGN KEY (Room_Reservation_RoomID)
+    REFERENCES Room (RoomID) ON DELETE NO ACTION;
